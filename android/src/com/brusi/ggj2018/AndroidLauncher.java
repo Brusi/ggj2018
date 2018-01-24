530f04f1932c7c44315019b41fbc23ab5f29bf67
Ori <bbrusi@gmail.com>
2018-01-24 21:11:23 +0200

    Build initial project.

---
+++ android/src/com/brusi/ggj2018/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brusi.ggj2018;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brusi.ggj2018.ggj2018;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ggj2018(), config);
+	}
+}
---
+++ desktop/src/com/brusi/ggj2018/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brusi.ggj2018.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brusi.ggj2018.ggj2018;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ggj2018(), config);
+	}
+}
